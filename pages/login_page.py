d6b062eac6c46e2dec959aad5b502cb2095138dc
Irishka Ermolovich <irishka.131290@gmail.com>
2019-11-03 23:16:52 +0300

    add transition between pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -8,14 +9,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert self.browser.current_url == "http://selenium1py.pythonanywhere.com/ru/accounts/login/", "link is not correct"
+        assert self.browser.current_url == "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/", "link is not correct "
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
-
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
